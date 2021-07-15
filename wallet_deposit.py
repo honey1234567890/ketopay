b496a067b5d34ff8994501b8d0ae8ed8257a9293
honey1234567890 <87442530+honey1234567890@users.noreply.github.com>
2021-07-15 11:27:46 +0530

    Update wallet_deposit.py

--- wallet_deposit.py
+++ wallet_deposit.py
@@ -15,12 +15,12 @@ db=firebase.database()
 def walletDeposit(amount,country,currency,customer_id):
  checkout_page={
     "amount": amount,
-    "complete_payment_url": "http://mediapipe-com.stackstaging.com/",
-    'complete_checkout_url': "http://mediapipe-com.stackstaging.com/",
+    "complete_payment_url": "https://ketopayment.herokuapp.com/sendsuccess",
+    'complete_checkout_url': "https://ketopayment.herokuapp.com/sendsuccess",
     "country": country,
     "currency": currency,
     "customer": customer_id,
-    "error_payment_url": "http://www.rapyd.net",
+    "error_payment_url": "https://ketopayment.herokuapp.com/dashboard",
     "merchant_reference_id": "950ae8c6-79",
     "language": "en",
     "metadata": {
